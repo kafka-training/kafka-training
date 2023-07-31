601478407fcbfe2d03076f2a8c4ec836f32d5962
gabor <your.email@example.com>
2023-07-31 11:34:36 +0200

    u

---
+++ java/Hello/HelloApp/src/App.java
@@ -0,0 +1,7 @@
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Apple apple = new Apple();
+        apple.sayHello();
+    }
+}
---
+++ java/Hello/HelloApp/src/Apple.java
@@ -0,0 +1,5 @@
+public class Apple {
+    public void sayHello() {
+        System.out.println("Hello, World!");
+    }   
+}
